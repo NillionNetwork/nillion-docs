b541168d6acc8376afbd0f6b735389f438a9ffa4
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2025-01-16 14:56:18 +0100

    chore: latest refactor on api

--- sidebar-api.js
+++ sidebar-api.js
@@ -13,15 +13,15 @@ module.exports = [
       'api/nildb/overview',
       'api/nildb/get-health-status', // Health
       'api/nildb/get-node-details', // About
-      'api/nildb/retrieve-an-organizations-account-details',
+      'api/nildb/retrieve-an-organizations-account-details', // Accounts
       {
         type: 'category',
-        label: 'List Schemas',
+        label: 'Schemas',
         items: [
-          'api/nildb/upload-data-to-the-specified-schema-collection', // Upload / Read
-          'api/nildb/retrieve-data-from-the-specified-schema-collection-that-matches-the-provided-filter', // XXX
-          'api/nildb/retrieve-recently-added-documents-from-a-schema-collection', // XXX
-          'api/nildb/update-documents-within-a-schema-collection-that-match-the-given-filter', // XXX
+          'api/nildb/upload-data-to-the-specified-schema-collection', // Upload /
+          'api/nildb/retrieve-data-from-the-specified-schema-collection-that-matches-the-provided-filter', // Read
+          'api/nildb/retrieve-recently-added-documents-from-a-schema-collection', // Tail
+          'api/nildb/update-documents-within-a-schema-collection-that-match-the-given-filter', // Update
           'api/nildb/delete-data-records-that-match-a-given-filter', // Detail
           'api/nildb/remove-all-documents-in-a-schema-collection', // Flush
           'api/nildb/list-the-organizations-schemas', // List
@@ -29,7 +29,7 @@ module.exports = [
       },
       {
         type: 'category',
-        label: 'List Queries',
+        label: 'Queries',
         items: [
           'api/nildb/list-the-organizations-queries', // List
           'api/nildb/execute-the-specified-query', // Execute
