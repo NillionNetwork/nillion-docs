b02531d1273c6334d66621af99f0755d336dae82
crypblizz <tom.terado@nillion.com>
2025-07-07 12:08:36 +0200

    chore: test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -264,6 +264,35 @@ const config = {
         },
       },
     ],
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
+        toExtensions: ['exe', 'zip'], // /myAsset -> /myAsset.zip (if latter exists)
+        redirects: [
+          // /docs/oldDoc -> /docs/newDoc
+          {
+            to: '/docs/build/private-storage/overview',
+            from: '/docs/build/secret-vault',
+          },
+          // Redirect from multiple old paths to the new path
+          // {
+          //   to: '/docs/newDoc2',
+          //   from: ['/docs/oldDocFrom2019', '/docs/legacyDocFrom2016'],
+          // },
+        ],
+        //   createRedirects(existingPath) {
+        //     if (existingPath.includes('/community')) {
+        //       // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
+        //       return [
+        //         existingPath.replace('/community', '/docs/team'),
+        //         existingPath.replace('/community', '/docs/support'),
+        //       ];
+        //     }
+        //     return undefined; // Return a falsy value: no redirect created
+        //   },
+      },
+    ],
   ],
 };
 
