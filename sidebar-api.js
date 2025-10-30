8dbe6127ed81488aa28140eb15faef65c108597a
psofiterol <gpentafragkas@protonmail.com>
2025-10-30 15:31:05 +0200

    chore: remove legacy nilDB API section

--- sidebar-api.js
+++ sidebar-api.js
@@ -5,54 +5,6 @@ module.exports = [
     value: 'API',
     defaultStyle: true,
   },
-  'api/overview',
-  {
-    type: 'category',
-    label: 'nilDB API',
-    items: [
-      'api/nildb/overview',
-      'api/nildb/accounts',
-      {
-        type: 'category',
-        label: 'Node',
-        items: [
-          'api/nildb/get-health-status', // Health
-          'api/nildb/get-node-details', // Details
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Schemas',
-        items: [
-          'api/nildb/get-schemas', // List Schemas
-          'api/nildb/add-schema', // Add Schema
-          'api/nildb/delete-schema', // Delete Schema
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Data',
-        items: [
-          'api/nildb/upload-data', // Upload
-          'api/nildb/read-data', // Read
-          'api/nildb/list-new-data', // Tail
-          'api/nildb/update-data', // Update
-          'api/nildb/delete-data', // Detail
-          'api/nildb/delete-all-data', // Flush
-        ],
-      },
-      {
-        type: 'category',
-        label: 'Queries',
-        items: [
-          'api/nildb/get-queries', // List Query
-          'api/nildb/execute-query', // Execute Query
-          'api/nildb/add-query', // Add Query
-          'api/nildb/delete-query', // Delete Query
-        ],
-      },
-    ],
-  },
   {
     type: 'category',
     label: 'nilAI API',
