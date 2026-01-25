fc864e494a72a6dae1ec8071d7af96ebec7f3511
yinka <yunggenius4@gmail.com>
2026-01-25 15:54:09 +0100

    feat: Add MetamaskConnect component for wallet network setup
    Adds a styled button component that allows users to add the Nillion L2
    network to MetaMask with one click.

---
+++ src/components/MetamaskConnect/index.js
@@ -0,0 +1,33 @@
+import styles from './styles.module.css';
+
+export default function AddNetworkButton({ chainId, chainName, rpcUrl, symbol, explorer }) {
+  const addNetwork = async () => {
+    if (!window.ethereum) {
+      window.open('https://metamask.io/download/', '_blank');
+      return;
+    }
+
+    const chainIdHex = '0x' + parseInt(chainId).toString(16);
+
+    try {
+      await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+          chainId: chainIdHex,
+          chainName,
+          nativeCurrency: { name: symbol, symbol, decimals: 18 },
+          rpcUrls: [rpcUrl],
+          blockExplorerUrls: explorer ? [explorer] : []
+        }]
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <button className={styles.connectButton} onClick={addNetwork}>
+      Add {chainName} to MetaMask
+    </button>
+  );
+}
